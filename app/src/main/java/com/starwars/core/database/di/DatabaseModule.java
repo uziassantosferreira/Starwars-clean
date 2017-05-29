38226536f76105c51684ba92973c725dc1606f53
Uzias Santos <uziasferreira@gmail.com>
2017-05-29 09:13:35 -0300

    chore: create database module

---
+++ app/src/main/java/com/starwars/core/database/di/DatabaseModule.java
@@ -0,0 +1,56 @@
+package com.starwars.core.database.di;
+
+import android.app.Application;
+
+import com.starwars.BuildConfig;
+
+import java.util.concurrent.Executors;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import io.requery.Persistable;
+import io.requery.android.sqlite.DatabaseSource;
+import io.requery.meta.EntityModel;
+import io.requery.reactivex.ReactiveEntityStore;
+import io.requery.reactivex.ReactiveSupport;
+import io.requery.sql.Configuration;
+import io.requery.sql.ConfigurationBuilder;
+import io.requery.sql.EntityDataStore;
+import io.requery.sql.TableCreationMode;
+
+@Module
+@Singleton
+public class DatabaseModule {
+
+    @Provides
+    ReactiveEntityStore<Persistable> provideDatabase(Configuration configuration) {
+        return ReactiveSupport.toReactiveStore(new EntityDataStore<Persistable>(configuration));
+    }
+
+    @Provides
+    Configuration provideConfiguration(DatabaseSource source, EntityModel entityModel) {
+        return new ConfigurationBuilder(source, entityModel)
+                .useDefaultLogging()
+                .setWriteExecutor(Executors.newSingleThreadExecutor())
+                .build();
+    }
+
+    @Provides
+    EntityModel provideModels() {
+        //TODO CREATE FIRST CLASS WITH ANNOTATION ENTITY TO GENERATE MODELS
+        return null;
+        //return Models.DEFAULT;
+    }
+
+    @Provides
+    DatabaseSource provideDatabaseSource(Application application, EntityModel entityModel) {
+        final DatabaseSource source = new DatabaseSource(application, entityModel, 1);
+        if (BuildConfig.DEBUG) {
+            source.setTableCreationMode(TableCreationMode.DROP_CREATE);
+        }
+        return source;
+    }
+
+}
